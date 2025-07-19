f231506a5462fc2afbcc36878624128a82d6e842
tammieoh <oht@oxy.edu>
2025-07-19 09:20:10 -0700

    create POST endpoint and in-memory storage of tracks

--- src/main/java/com/musicproject/toptracks/TrackController.java
+++ src/main/java/com/musicproject/toptracks/TrackController.java
@@ -1,20 +1,27 @@
 package com.musicproject.toptracks;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TrackController {
 
+    List<Track> tracks = new ArrayList<>();
+
     // create a GET endpoint
     @GetMapping("/tracks")
     public List<Track> getTrack(){
-        // returns 3 hard coded tracks as JSON
-        var t1 = new Track("Sicko Mode", "Travis Scott", "Hip Hop", 20);
-        var t2 = new Track("Driver's License", "Olivia Rodrigo", "Pop", 15);
-        var t3 = new Track("Humble", "Kendrick Lamar", "Hip Hop", 50);
-        return List.of(t1, t2, t3);
+        return tracks;
+    }
+
+    @PostMapping("/tracks")
+    public void createTrack(@RequestBody Track track){
+        System.out.println(track.getTitle());
+        tracks.add(track);
     }
 }
